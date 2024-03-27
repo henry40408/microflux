40089aea85cdc0381cf3f9e04076d45a2cd8f859
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-03-28 00:25:02 +0800

    chore: develop with nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "microflux";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShellNoCC {
+          packages = with pkgs; [ nodejs_20 zsh ];
+          shellHook = ''
+            exec zsh
+          '';
+        };
+      });
+    };
+}
