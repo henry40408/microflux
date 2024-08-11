3a4a2ac198e1c81d7959321332d549488ce03ef8
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-08-11 19:37:39 +0800

    feat: get full url for summary

--- components/SummarizeButton.vue
+++ components/SummarizeButton.vue
@@ -3,7 +3,8 @@ import { secondsToMilliseconds } from "date-fns";
 
 import type { KagiSummarizeResponse } from "../types";
 
-const model = defineModel<string>();
+const model = defineModel<string>("summary");
+const finalUrl = defineModel<string>("finalUrl");
 const props = defineProps<{ url: string }>();
 
 const { data, status, error, execute, clear } =
@@ -17,23 +18,51 @@ const { data, status, error, execute, clear } =
     watch: false,
   });
 
+const {
+  data: fullUrlData,
+  status: fullUrlStatus,
+  error: fullUrlError,
+  execute: executeFullUrl,
+  clear: clearFullUrl,
+} = await useLazyFetch<{
+  url: string;
+}>("/api/full-url", {
+  key: `full-url-${props.url}`,
+  query: { url: props.url },
+  immediate: false,
+  server: false,
+  timeout: secondsToMilliseconds(30),
+  watch: false,
+});
+
+const isPending = computed(
+  () => status.value === "pending" || fullUrlStatus.value === "pending",
+);
+const isSuccess = computed(
+  () => status.value === "success" && fullUrlStatus.value === "success",
+);
+
 async function onClick() {
-  await execute();
+  await Promise.all([execute(), executeFullUrl()]);
   if (data.value) model.value = data.value.output_data.markdown;
+  console.log(fullUrlData.value);
+  if (fullUrlData.value) finalUrl.value = fullUrlData.value.url;
 }
 
 function onCancel() {
   clear();
+  clearFullUrl();
   model.value = "";
+  finalUrl.value = "";
 }
 </script>
 
 <template>
   <MyButton
     :cancel="onCancel"
-    :done="status === 'success'"
-    :error="error"
-    :loading="status === 'pending'"
+    :done="isSuccess"
+    :error="error || fullUrlError"
+    :loading="isPending"
     @click="onClick"
     >summarize<template #done>reset summary</template></MyButton
   >
