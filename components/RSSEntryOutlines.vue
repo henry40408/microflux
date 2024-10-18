410870f856a74d8d8905013d6d968403be5db2a8
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-10-19 01:53:42 +0800

    feat: filter entries by dates

--- components/RSSEntryOutlines.vue
+++ components/RSSEntryOutlines.vue
@@ -1,35 +1,6 @@
 <template>
   <details>
-    <summary>freshness, feeds &amp; categories</summary>
-    <p>
-      <span class="outline-items">
-        <span class="outline-item">freshness</span>
-        <span class="outline-item"
-          ><input
-            :id="noneId"
-            v-model="freshness"
-            type="radio"
-            :value="undefined"
-          /><label :for="noneId">none</label></span
-        >
-        <span class="outline-item"
-          ><input
-            :id="todayId"
-            v-model="freshness"
-            type="radio"
-            value="today"
-          /><label :for="todayId">today</label></span
-        >
-        <span class="outline-item"
-          ><input
-            :id="yesterdayId"
-            v-model="freshness"
-            type="radio"
-            value="yesterday"
-          /><label :for="yesterdayId">yesterday</label></span
-        >
-      </span>
-    </p>
+    <summary>dates, feeds &amp; categories</summary>
     <p>
       <span class="outline-items">
         <span class="outline-item">categories</span>
@@ -58,38 +29,59 @@
         <i v-if="!feeds.length" class="outline-item">no feeds</i>
       </span>
     </p>
+    <p>
+      <span class="outline-items">
+        <span class="outline-item">
+          date<sup>{{ timezone }}</sup>
+        </span>
+        <span v-for="group in dates" :key="group.date" class="outline-item">
+          <BaseButton @click="$emit('selectDate', group.date)">{{
+            group.date
+          }}</BaseButton>
+          <sup>{{ group.count }}</sup>
+        </span>
+      </span>
+    </p>
   </details>
 </template>
 
 <script setup lang="ts">
+import { formatISO } from "date-fns";
 import lodash from "lodash";
 import type {
   MinifluxCompactCategory,
   MinifluxCompactEntry,
 } from "~/server/trpc/routers/miniflux";
 
-const noneId = useId();
-const todayId = useId();
-const yesterdayId = useId();
-
 const model = defineModel<MinifluxCompactEntry[]>({ required: true });
 const emit = defineEmits<{
   selectCategory: [id: number];
+  selectDate: [date?: string];
   selectFeed: [id: number];
-  selectFreshness: [type?: string];
 }>();
 
-const props = defineProps<{ freshness?: string }>();
-const freshness = ref(props.freshness);
+const props = defineProps<{ date?: string }>();
+const date = ref(props.date);
 watch(
-  () => props.freshness,
-  () => (freshness.value = props.freshness),
+  () => props.date,
+  () => (date.value = props.date),
 );
 watch(
-  () => freshness.value,
-  () => emit("selectFreshness", freshness.value),
+  () => date.value,
+  () => emit("selectDate", date.value),
 );
 
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const dates = computed(() =>
+  lodash(model.value)
+    .groupBy((e) => formatISO(e.published_at, { representation: "date" }))
+    .map((entries) => ({
+      date: formatISO(entries[0].published_at, { representation: "date" }),
+      count: entries.length,
+    }))
+    .orderBy([(g) => g.count, (g) => g.date], ["desc", "asc"])
+    .value(),
+);
 const feeds = computed(() =>
   lodash(model.value)
     .groupBy("feed.id")
