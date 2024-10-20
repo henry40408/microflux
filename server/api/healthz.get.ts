b3fef5c9ccd238493355a697b3f2259c9577b4f7
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-10-20 22:30:58 +0800

    fix: remove git commit hash from healthz route

--- server/api/healthz.get.ts
+++ server/api/healthz.get.ts
@@ -1,5 +1,3 @@
-import { getGitCommitHash } from "~/utils/get-git-commit-hash";
-
 export default defineEventHandler(async (_event) => {
-  return { status: "OK", gitCommitHash: getGitCommitHash() };
+  return { status: "OK" };
 });
